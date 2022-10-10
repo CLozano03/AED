a5086ac97de7615ff1c72e16a0bea23f4ad06486
SesarLosano <cesar.lozano.argueso@gmail.com>
2022-10-10 12:15:59 +0200

    Añadidos algunos commentarios en filter

--- P3_filter/Utils.java
+++ P3_filter/Utils.java
@@ -8,15 +8,19 @@ import java.util.function.Predicate;
 public class Utils {
 
   public static <E> Iterable<E> filter(Iterable<E> d, Predicate<E> pred) {
+    //Creamos una indexedList para almacenar la informacion del nuevo iterable
     IndexedList<E> result = new ArrayIndexedList<E>();
     Iterator<E> iterator = d.iterator();
     
     while (iterator.hasNext()) {
+      //Guardo mi elemeto
       E e = iterator.next();
+      
+      //Aplico los filtros y aniado en caso de que los pase
       if(e != null && pred.test(e)){
         result.add(result.size(), e);
       }
     }
-    return result;
+    return (Iterable<E>)result;
   }
 }
