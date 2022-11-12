fc09cc2c7cae1b0d4d002e4b2df9f7091a48a912
SesarLosano <cesar.lozano.argueso@gmail.com>
2022-11-12 19:57:42 +0100

    Added method joinMultiSets and tested

--- L4_Recursividad/Utils.java
+++ L4_Recursividad/Utils.java
@@ -41,6 +41,34 @@ public class Utils {
   public static <E extends Comparable<E>> NodePositionList<Pair<E,Integer>>
     joinMultiSets(NodePositionList<Pair<E,Integer>> l1,
 		  NodePositionList<Pair<E,Integer>> l2) {
-    return null;
+        NodePositionList<Pair<E,Integer>> res = new NodePositionList<Pair<E,Integer>>();
+      return joinMultiSetsAux(l1, l2, res, l1.first(), l2.first());
   }
+
+  public static <E extends Comparable<E>> NodePositionList<Pair<E,Integer>>
+    joinMultiSetsAux(NodePositionList<Pair<E,Integer>> l1,
+		  NodePositionList<Pair<E,Integer>> l2, NodePositionList<Pair<E,Integer>> res, Position<Pair<E,Integer>> cursor1, Position<Pair<E,Integer>> cursor2) {
+        if(cursor1 == null && cursor2 == null) {
+          return res;
+        } else if(cursor1 == null) {
+          res.addLast(cursor2.element());
+          return joinMultiSetsAux(l1,l2, res, cursor1, l2.next(cursor2));
+        } else if (cursor2 == null){
+          res.addLast(cursor1.element());
+          return joinMultiSetsAux(l1, l2, res, l1.next(cursor1), cursor2);
+        } else if(cursor1.element().getLeft().compareTo(cursor2.element().getLeft()) == 0){
+          Pair<E,Integer> el1 = cursor1.element();
+          Pair<E,Integer> el2 = cursor2.element();
+
+          res.addLast(el1);
+          res.last().element().setRight(el1.getRight() + el2.getRight());
+          return joinMultiSetsAux(l1, l2, res, l1.next(cursor1), l2.next(cursor2));
+        } else if(cursor1.element().getLeft().compareTo(cursor2.element().getLeft()) < 0){
+          res.addLast(cursor1.element());
+          return joinMultiSetsAux(l1, l2, res, l1.next(cursor1), cursor2);
+        } else {
+          res.addLast(cursor2.element());
+          return joinMultiSetsAux(l1, l2, res, cursor1, l2.next(cursor2));
+        }
+    }
 }
