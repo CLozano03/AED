aec5d8e68249ef976f43682161abef2e73bddd1c
SesarLosano <cesar.lozano.argueso@gmail.com>
2022-11-16 11:38:46 +0100

    Implementada solucion Explorador.java

--- L4_Recursividad/Explorador.java
+++ L4_Recursividad/Explorador.java
@@ -1,5 +1,6 @@
 package aed.recursion;
 
+
 import es.upm.aedlib.Pair;
 import es.upm.aedlib.positionlist.*;
 
@@ -7,6 +8,26 @@ import es.upm.aedlib.positionlist.*;
 public class Explorador {
   
   public static Pair<Object,PositionList<Lugar>> explora(Lugar inicialLugar) {
-    return null;
+    return exploraAux(inicialLugar, new NodePositionList<Lugar>());
+  }
+
+  public static Pair<Object,PositionList<Lugar>> exploraAux(Lugar inicialLugar, NodePositionList<Lugar> camino){
+    if(inicialLugar == null) {
+      return new Pair<Object,PositionList<Lugar>>(null, camino);
+    } 
+    
+    if(inicialLugar.tieneTesoro()){
+      camino.addLast(inicialLugar);
+      return new Pair<Object,PositionList<Lugar>>(inicialLugar.getTesoro(), camino);
+    } else {
+      inicialLugar.marcaSueloConTiza();
+      for(Lugar lugar: inicialLugar.caminos()){
+        if(!lugar.sueloMarcadoConTiza()){
+          camino.addLast(inicialLugar);
+            return exploraAux(lugar, camino);
+          }
+      }
+      return exploraAux(camino.remove(camino.last()), camino);
+    }
   }
 }
