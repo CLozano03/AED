641a3ba510947927b24dbd653bd47e7844199214
viiiccv <95257527+viiiccv@users.noreply.github.com>
2022-12-05 02:39:30 +0100

    Empezado un poco

--- L6_PriorityQueues/UrgenciasAED.java
+++ L6_PriorityQueues/UrgenciasAED.java
@@ -1,5 +1,7 @@
 package aed.urgencias;
 
+import java.util.Iterator;
+import es.upm.aedlib.Entry;
 import es.upm.aedlib.Pair;
 import es.upm.aedlib.map.HashTableMap;
 import es.upm.aedlib.priorityqueue.HeapPriorityQueue;
@@ -16,13 +18,32 @@ public class UrgenciasAED implements Urgencias {
 
     @Override
     public Paciente admitirPaciente(String DNI, int prioridad, int hora) throws PacienteExisteException {
-        Paciente paciente = new Paciente(DNI, prioridad, 0,0);
+        Paciente paciente = new Paciente(DNI, prioridad, hora, hora);
+        cola.enqueue(prioridad, paciente); // Insertamos el nuevo nodo en la ultima posicion libre
+        // ¿violamos la heap order property?--> cada padre tiene mayo prioridad que su hijos
+        Iterator<Entry<Integer, Paciente>> it = cola.iterator();
+        while(it.hasNext()) {
+            Entry<Integer, Paciente> entry = it.next();
+            Paciente pac = entry.getValue();
+            if (pac.compareTo(paciente) > 0) { // es mas urgente atender a paciente 
+                //Los voy a intercambiar
+                Integer clavePac = entry.getKey();
+                Paciente pacientePac = entry.getValue();
+                // Pongo en su lugar a paciente
+                replaceKey​(entry, prioridad);
+                replaceValue​(entry, paciente);
+                paciente = pac;
+            } else if (pac.compareTo(paciente) < 0) {
+                replaceKey​(entry,pac.getPrioridad());
+                replaceValue​(entry, pac);
+            }
+        }
         return lista.put(DNI, paciente);
     }
 
     @Override
     public Paciente salirPaciente(String DNI, int hora) throws PacienteNoExisteException {
-        // TODO Auto-generated method stub
+        
         return null;
     }
 
