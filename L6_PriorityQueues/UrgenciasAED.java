7522a15b93f326d6705a3d2e08938ff32be146e1
SesarLosano <cesar.lozano.argueso@gmail.com>
2022-12-08 14:13:37 +0100

    Cambios en la estructura del programa

--- L6_PriorityQueues/UrgenciasAED.java
+++ L6_PriorityQueues/UrgenciasAED.java
@@ -12,7 +12,7 @@ import es.upm.aedlib.priorityqueue.HeapPriorityQueue;
 public class UrgenciasAED implements Urgencias {
     
     private HeapPriorityQueue<Integer, Paciente> cola;
-    private HashTableMap<String, Paciente> lista; 
+    private HashTableMap<String, Entry<Integer, Paciente>> lista; 
 
     private int pacientesAtendidos;
     private int sumaTiemposAdmision;
@@ -32,105 +32,60 @@ public class UrgenciasAED implements Urgencias {
         if (getPaciente(DNI) != null) {
             throw new PacienteExisteException();
         }
-        cola.enqueue(prioridad, paciente); // Insertamos el nuevo nodo en la ultima posicion libre
-        // ¿violamos la heap order property?--> cada padre tiene mayor prioridad que su hijos
-        /* Iterator<Entry<Integer, Paciente>> it = cola.iterator();
-        while(it.hasNext()) {
-            Entry<Integer, Paciente> entry = it.next();
-            Paciente pac = entry.getValue();
-            if (pac.compareTo(paciente) > 0) { // es mas urgente atender a paciente 
-                //Los voy a intercambiar
-                Integer clavePac = entry.getKey();
-                Paciente pacientePac = entry.getValue();
-                // Pongo en su lugar a paciente
-                cola.replaceKey(entry, prioridad);
-                cola.replaceValue(entry, paciente);
-                paciente = pac;
-            } else if (pac.compareTo(paciente) < 0) {
-                cola.replaceKey(entry, pac.getPrioridad());
-                cola.replaceValue(entry, pac);
-            }
-        } */
-        lista.put(DNI, paciente);
-        return paciente;
+        lista.put(DNI, cola.enqueue(prioridad, paciente));
+        return lista.get(DNI).getValue();
     }
 
     @Override
     /* Paciente sale de las urgencias (sin ser atendido). 
         El paciente se borra de la estructuras de datos de las urgencias */
     public Paciente salirPaciente(String DNI, int hora) throws PacienteNoExisteException {
-        /* // Compruebo que Paciente existe en la cola
-        Paciente pacBus = new Paciente(DNI, 0, hora, hora);
-        Iterator<Entry<Integer, Paciente>> it = cola.iterator();
-        boolean found = false;
-        Entry<Integer, Paciente> entry;
-        while(it.hasNext() && !found) {
-            entry = it.next();
-            Paciente pac = entry.getValue();
-            if (pac.equals(pacBus)) {
-                found = true;
-                pacBus = new Paciente(pac.getDNI(), pac.getPrioridad(), pac.getTiempoAdmision(), pac.getTiempoAdmisionEnPrioridad());
-            }
-        }
-        if (!found) {
+        if (lista.get(DNI) == null) {
             throw new PacienteNoExisteException();
         }
-        Entry<Integer, Paciente> entryPac = new EntryImpl<Integer,Paciente>(pacBus.getPrioridad(), pacBus);
-        */
-        Paciente paciente = lista.get(DNI);
-        if (paciente == null) {
-            throw new PacienteNoExisteException();
-        }
-        cola.remove(new EntryImpl<Integer, Paciente>(paciente.getPrioridad(), paciente));
+        cola.remove(lista.get(DNI));
         /* Lo borro del hashMap y returneo*/
-        return lista.remove(DNI);
+        return lista.remove(DNI).getValue();
     }
 
     @Override
     public Paciente cambiarPrioridad(String DNI, int nuevaPrioridad, int hora) throws PacienteNoExisteException {
-        //Paciente buscoPac = new Paciente(DNI, 0,0,0);
-        
-        // Compruebo que el paciente existe
         Paciente buscoPac = getPaciente(DNI);
         if (buscoPac == null) {
             throw new PacienteNoExisteException();
         }
-        /* Iterator<Entry<Integer, Paciente>> it = cola.iterator();
-        boolean found = false;
-        while(it.hasNext() && !found) {
-            Entry<Integer, Paciente> entry = it.next();
-            if (buscoPac.equals(entry.getValue())) {
-                found = true;
-                lista.remove(buscoPac.getDNI());
-                buscoPac = entry.getValue();
-                buscoPac.setPrioridad(nuevaPrioridad);
-                buscoPac.setTiempoAdmisionEnPrioridad(hora);
-                lista.put(buscoPac.getDNI(), buscoPac); //lo annado de nuevo a la lista actualizado
-            }
-        } */
-        Entry<Integer, Paciente> entryQuitar = new EntryImpl<Integer,Paciente>(buscoPac.getPrioridad(), buscoPac);
-        cola.remove(entryQuitar);
+        if(buscoPac.getPrioridad() == nuevaPrioridad){
+            return buscoPac;
+        }
+        cola.remove(lista.get(DNI));
         lista.remove(DNI); //lo quito de la lista porque va a cambiar
         buscoPac.setPrioridad(nuevaPrioridad);
         buscoPac.setTiempoAdmisionEnPrioridad(hora);
-        cola.enqueue(buscoPac.getPrioridad(), buscoPac);
-    
          //lo annado de nuevo a la lista actualizado
-        return lista.put(DNI, buscoPac);
+         lista.put(DNI, cola.enqueue(buscoPac.getPrioridad(), buscoPac));
+        return lista.get(DNI).getValue();
     }
 
     @Override
     public Paciente atenderPaciente(int hora) {
+        if(cola.isEmpty()){
+            return null;
+        }
         pacientesAtendidos++;
-        Paciente pacienteAtendido = cola.dequeue().getValue();
+        Paciente pacienteAtendido = cola.first().getValue();
         sumaTiemposAdmision += hora - pacienteAtendido.getTiempoAdmision(); 
-        return lista.remove(pacienteAtendido.getDNI()); 
+        return lista.remove(cola.dequeue().getValue().getDNI()).getValue(); 
     }
 
     @Override
     public void aumentaPrioridad(int maxTiempoEspera, int hora) {
-        for(Entry<Integer, Paciente> entry: cola){
-
+        for(Entry<String, Entry<Integer, Paciente>> entry: lista){
+            Paciente paciente = entry.getValue().getValue();
+            if(hora - paciente.getTiempoAdmision() > maxTiempoEspera ){
+                try {
+                    cambiarPrioridad(entry.getKey(), paciente.getPrioridad() -1, hora);
+                } catch (PacienteNoExisteException e) {}
+            }
         }
     }
 
@@ -138,8 +93,8 @@ public class UrgenciasAED implements Urgencias {
     public Iterable<Paciente> pacientesEsperando() {
         Paciente paciente;
         IndexedList<Paciente> listaOrdenada = new ArrayIndexedList<>();
-        for(Entry<String,Paciente> entry: lista){
-            paciente = entry.getValue();
+        for(Entry<String, Entry<Integer, Paciente>> entry: lista){
+            paciente = entry.getValue().getValue();
             insertar(paciente, listaOrdenada);
         }
         return listaOrdenada;
@@ -147,7 +102,11 @@ public class UrgenciasAED implements Urgencias {
 
     @Override
     public Paciente getPaciente(String DNI) {
-        return lista.get(DNI);
+        Entry<Integer, Paciente> entry = lista.get(DNI);
+        if(entry == null){
+            return null;
+        }
+        return entry.getValue();
     }
 
     @Override
